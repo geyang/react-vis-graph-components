c670e2f1078a85bcbc46bc2b902fad840aef1493
Ge Yang <ge@uber.com>
2016-08-24 15:03:57 -0700

    use outline instead

--- src/connectors/bezier-connector.js
+++ src/connectors/bezier-connector.js
@@ -32,36 +32,49 @@ export default function BezierConnector({
   };
 
   const line = new Bezier(x1, y1, x1 + bezierOffset, y1, x2 - bezierOffset, y2, x2, y2);
-  let shapes;
+
+  let d;
   try {
-    shapes = line.outlineshapes(width/2);
+    d = line.outline(width / 2).curves.map(
+        bezier => bezier.toSVG().replace('M', 'L')
+      ).join(' ').replace(/^L/i, 'M');
   } catch (e) {
     // if points are degenerate, fall back on regular path
+    console.warn(e);
     return <path d="" {...props}/>
   }
 
-  const ds = shapes.map(
-    ({startcap, forward, endcap, back}) => {
-      return [startcap, forward, endcap, back].map(
-        l => l.toSVG().replace('M', 'L')
-      ).join(' ').replace('L', 'M');
-    }
-  );
+  console.log(d);
 
-  return (
-    <g>
-      {ds.map(
-        (d, ind) => {
-          return (
-            <path
-              key={ind}
-              d={d}
-              {...props}/>
-          );
-        }
-      )}
-    </g>
-  );
+  if (d) {
+    return (
+      <path {...props} d={d}/>
+    );
+  }
+  return <path d="" {...props}/>
+
+  // const ds = shapes.map(
+  //   ({startcap, forward, endcap, back}) => {
+  //     return [startcap, forward, endcap, back].map(
+  //       l => l.toSVG().replace('M', 'L')
+  //     ).join(' ').replace('L', 'M');
+  //   }
+  // );
+
+  // return (
+  //   <g>
+  //     {ds.map(
+  //       (d, ind) => {
+  //         return (
+  //           <path
+  //             key={ind}
+  //             d={d}
+  //             {...props}/>
+  //         );
+  //       }
+  //     )}
+  //   </g>
+  // );
 }
 
 BezierConnector.graphNodeType = NODE_TYPES.LINK;
