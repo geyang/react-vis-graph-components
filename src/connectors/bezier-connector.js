de4ac99f9b6ced5ea56e27ac2257055dd36844a7
Ge Yang <yangge1987@gmail.com>
2016-08-24 23:22:50 -0700

    handle bezier curve corner case

--- src/connectors/bezier-connector.js
+++ src/connectors/bezier-connector.js
@@ -36,12 +36,13 @@ export default function BezierConnector({
   let d;
   try {
     d = line.outline(width / 2).curves.map(
-        bezier => bezier.toSVG().replace('M', 'L')
-      ).join(' ').replace(/^L/i, 'M');
+      bezier => bezier.toSVG().replace('M', 'L')
+    ).join(' ').replace(/^L/i, 'M');
   } catch (e) {
     // if points are degenerate, fall back on regular path
-    console.warn(e);
-    return <path d="" {...props}/>
+    return <path d={
+    `M ${x1} ${y1 + width / 2} L ${x2} ${y2 + width / 2}
+    L ${x2} ${y2 - width / 2} L ${x1} ${y1 - width / 2} Z`} {...props}/>
   }
 
   if (d) {
