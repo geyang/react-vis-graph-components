d73dbde85cdac028ba2b5b1e65b7770159e02c51
Ge Yang <ge@uber.com>
2016-09-02 11:22:19 -0700

    add tons of documentation

--- Readme.js
+++ Readme.js
@@ -17,9 +17,17 @@ var _reactHighlight = require('@episodeyang/react-highlight.js');
 
 var _reactHighlight2 = _interopRequireDefault(_reactHighlight);
 
+var _reactComponentPropsTable = require('react-component-props-table');
+
+var _reactComponentPropsTable2 = _interopRequireDefault(_reactComponentPropsTable);
+
 var _linkGraphSimple = require('./link-graph-simple.example');
 
 var _linkGraphSimple2 = _interopRequireDefault(_linkGraphSimple);
+
+var _linkGraph = require('!!react-docgen!../link-graph');
+
+var _linkGraph2 = _interopRequireDefault(_linkGraph);
 
 var _linkGraphSimpleExample = require('!!raw!./link-graph-simple.example.js');
 
@@ -28,6 +36,10 @@ var _linkGraphSimpleExample2 = _interopRequireDefault(_linkGraphSimpleExample);
 var _sankeySimple = require('./sankey-simple.example');
 
 var _sankeySimple2 = _interopRequireDefault(_sankeySimple);
+
+var _sankey = require('!!react-docgen!../sankey');
+
+var _sankey2 = _interopRequireDefault(_sankey);
 
 var _sankeySimpleExample = require('!!raw!./sankey-simple.example.js');
 
@@ -43,10 +55,7 @@ var _sankeyFullExample2 = _interopRequireDefault(_sankeyFullExample);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); } /**
-                                                                                                                   * Created on 6/24/16.
-                                                                                                                   */
-
+function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
 
 function Readme(_ref) {
   _objectDestructuringEmpty(_ref);
@@ -57,6 +66,7 @@ function Readme(_ref) {
     '\n      # React Vis Graph Component: Link Graph and Sankey Diagram\n\n      ## Linked Node Graph\n\n      [![github](https://img.shields.io/github/downloads/episodeyang/' + 'react-vis-graph-components/total.svg?style=flat-square&maxAge=2592000)]()\n\n      This component allows you to draw a linked node graph easily.\n      The graph component takes in the children (links and nodes) and\n      automatically calculates the end points for the connections and\n      adds padding.\n      ',
     _react2.default.createElement(_linkGraphSimple2.default, null),
     '\n      ### Usage Example\n\n      The source code below of the example above is loaded using the\n      webpack raw loader.',
+    _react2.default.createElement(_reactComponentPropsTable2.default, { propMetaData: _linkGraph2.default.props }),
     _react2.default.createElement(
       _reactHighlight2.default,
       null,
@@ -65,6 +75,7 @@ function Readme(_ref) {
     '\n\n      ## Sankey Diagram\n\n      ',
     _react2.default.createElement(_sankeySimple2.default, null),
     '\n      ### Simple Usage example\n\n      The source code below of the example above is loaded using the\n      webpack raw loader.',
+    _react2.default.createElement(_reactComponentPropsTable2.default, { propMetaData: _sankey2.default.props }),
     _react2.default.createElement(
       _reactHighlight2.default,
       null,
